ca7a54f8300819b9b5d0224322ba3c35aa632732
indira-h <indira.hagmann@stud.fhgr.ch>
2025-05-26 15:53:25 +0200

    aktualisiert 26.05.25 15:53

--- script.js
+++ script.js
@@ -20,16 +20,13 @@ async function zeigeParkhaus(name) {
     return;
   }
 
-  // Freie Plätze aktualisieren
   document.getElementById("freiePlaetze").innerText = `${parkhaus.shortfree}\nfreie Plätze`;
 
-  // Auslastung aktualisieren
   const auslastungEl = document.getElementById("auslastung");
   const belegung = parkhaus.belegung_prozent;
 
   auslastungEl.innerText = `${belegung ?? "--"}%\nAuslastung`;
 
-  // Alle möglichen Farben zuerst entfernen
   auslastungEl.classList.remove("green", "yellow", "red", "blue");
 
   if (belegung == null) {
@@ -42,12 +39,10 @@ async function zeigeParkhaus(name) {
     auslastungEl.classList.add("red");
   }
 
-  // Karte aktualisieren
   document.getElementById("karte").src =
     `https://maps.google.com/maps?q=${parkhaus.standort.lat},${parkhaus.standort.lon}&z=15&output=embed`;
 }
 
-// Buttons generieren
 ladeParkdaten().then((daten) => {
   const container = document.getElementById("button-container");
   container.innerHTML = "";
@@ -57,13 +52,10 @@ ladeParkdaten().then((daten) => {
     button.innerText = ph.phname;
 
     button.onclick = () => {
-      // Alle anderen Buttons zurücksetzen
       const alleButtons = container.querySelectorAll("button");
       alleButtons.forEach(btn => btn.classList.remove("active"));
-      // Aktiven Button markieren
       button.classList.add("active");
-
-      // Parkhausdaten anzeigen
+      
       zeigeParkhaus(ph.phname);
     };
 
